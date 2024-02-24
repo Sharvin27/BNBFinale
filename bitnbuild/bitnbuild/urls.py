8d16482e7cbfe9420fcf10c4ebff96b84ac152dd
Aryan Kyatham <kyathamaryan@gmail.com>
2024-02-25 02:00:42 +0530

    add inventory

--- bitnbuild/bitnbuild/urls.py
+++ bitnbuild/bitnbuild/urls.py
@@ -22,6 +22,9 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('inventory.urls')),
+    path('', include('yolo.urls')),
+        path('', include('resources.urls')),
+
 ]
 
 
