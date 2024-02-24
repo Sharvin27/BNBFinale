e1819bd692b2510f438244ae2a65b60c02e26fde
Aryan Kyatham <120780784+ARYANK-08@users.noreply.github.com>
2024-02-25 01:01:14 +0530

    Update urls.py

--- bitnbuild/bitnbuild/urls.py
+++ bitnbuild/bitnbuild/urls.py
@@ -22,7 +22,8 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('inventory.urls')),
-    path('', include('resources.urls')),
+    path('', include('yolo.urls')),
+        path('', include('resources.urls')),
 
 ]
 
